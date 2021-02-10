f9e827a3d0085a850b05b80a77dc2d63e7c5f7e2
faridfarhan28 <37871833+faridfarhan28@users.noreply.github.com>
2021-02-10 17:34:56 +0700

    Just add freq define
    i would to add freq define but i affraid to add LoRa_APRS_Tracker.cpp. Hope it can help you

--- src/pins.h
+++ src/pins.h
@@ -9,6 +9,11 @@
 #define OLED_SCL	22
 #define OLED_RST	16
 
+#define FREQ_SET	433775000	//set freq in Hz 9 digit
+#define SF_SET		12		//Spreading Factor
+#define CR_SET		5		//Coding Rate
+#define BW_SET		125E6		//Bandwith
+
 #ifdef TTGO_T_Beam_V0_7
 	#define GPS_RX	15
 	#define GPS_TX	12
